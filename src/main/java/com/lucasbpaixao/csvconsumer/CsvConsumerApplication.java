2f742e62282b9bf8f2b1c8081d330846a408ec9b
lucasbpaixao <lucasbpaixao@outlook.com>
2022-03-11 17:59:48 -0300

    Creating project

---
+++ src/main/java/com/lucasbpaixao/csvconsumer/CsvConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.lucasbpaixao.csvconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasbpaixao/csvconsumer/CsvConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasbpaixao.csvconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
